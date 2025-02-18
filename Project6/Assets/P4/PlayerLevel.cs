3f985a7eb9b110f61f86fb3725ddf9a8f258e8ec
spectreX123 <344428499@qq.com>
2025-02-18 03:07:29 -0500

    UpgradePart

---
+++ Project6/Assets/P4/PlayerLevel.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using static UnityEngine.Mathf;
+using Unity.VisualScripting;
+[CreateAssetMenu()]
+[Inspectable]
+public class PlayerLevel : ScriptableObject
+{
+    [NonSerialized] private float miningRate = 5;
+
+    public float GetMiningRate()
+    {
+        return this.miningRate;
+    }
+    public void SetMiningRate(float miningRate)
+    {
+        this.miningRate = miningRate;
+    }
+    [NonSerialized] private float DPS = 5;
+
+    public float GetDPS() => DPS;
+    public void SetDPS(float value)
+    {
+        DPS = value;
+    }
+
+    [NonSerialized] private float iron = 0;
+    [NonSerialized] private float gold = 0;
+    [NonSerialized] private float diamonds = 0;
+
+    public float GetIron() => FloorToInt(iron);
+    public float GetGold() => FloorToInt(gold);
+    public float GetDiamonds() => FloorToInt(diamonds);
+
+    public float IncreaseIron(float value) => iron += value;
+    public float IncreaseGold(float value) => gold += value;
+    public float IncreaseDiamonds(float value) => diamonds += value;
+    public float DecreaseIron(float value) => iron -= value;
+    public float DecreaseGold(float value) => gold -= value;
+    public float DecreaseDiamonds(float value) => diamonds -= value;
+}
